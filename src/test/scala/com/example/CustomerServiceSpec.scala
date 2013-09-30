48fdb453ef34c40a0c03fdcf98f8aa1ca6a8b5c4
jameshoare <james.hoare2@ntlworld.com>
2013-09-30 20:11:08 +0100

    more tests

--- src/test/scala/com/example/CustomerServiceSpec.scala
+++ src/test/scala/com/example/CustomerServiceSpec.scala
@@ -12,60 +12,27 @@ import com.example.rest.{CustomerServiceActor, CustomerService}
 /**
  * specs2 testing
  */
-class CustomerExampleSpec extends Specification with Directives with Specs2RouteTest with CustomerService {
+class CustomerExampleSpec extends Specification  with Specs2RouteTest with CustomerService {
 
 
   def actorRefFactory = system // connect the DSL to the test ActorSystem
 
-  val routeTestTimeout = RouteTestTimeout(2 second span)
-
-
-
-
-
-  /*val customerRoute =
-    get {
-      path("getCustomer") {
-        complete {
-          <html>
-            <body>
-              <h1>Say hello to <i>James</i>!</h1>
-            </body>
-          </html>
-        }
-      } ~
-        path("greeting") {
-          complete("Hi!")
-        }
-
-    }*/
 
   "The service" should {
     "return a greeting for GET requests to the customerGreeting path" in {
       Get("/customerGreeting") ~> customerRoutes ~> check {
-        entityAs[String] must contain("Hello James")
+        //entityAs[String] must contain("Hello James")
         status.toString() === "200 OK"
       }
     }
 
-    /* "return a 'Hi!' response for GET requests to /greeting" in {
-       Get("/greeting") ~> customerRoute ~> check {
-         entityAs[String] === "Hi!"
-       }
-     }
 
-     "leave GET requests to other paths unhandled" in {
-       Get("/addCustomer") ~> customerRoute ~> check {
-         handled must beFalse
-       }
-     }
+      "return a customer for GET requests to the customer path" in {
+        Get("/customer/1?usr=James&pwd=Hoare") ~> customerRoutes ~> check {
+          status.toString() === "200 OK"
+        }
+      }
 
-     "return a MethodNotAllowed error for PUT requests to the root path" in {
-       Put() ~> sealRoute(customerRoute) ~> check {
-         status === MethodNotAllowed
-         entityAs[String] === "HTTP method not allowed, supported methods: GET"
-       }
-     }
-   }*/
+
   }
 }
